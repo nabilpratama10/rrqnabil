3c4af386182efdbbb7d0007165aacce388c800bd
nabilpratama10 <nabilpratama9999@gmail.com>
2026-03-10 00:20:38 +0700

    Add files via upload

---
+++ latihansoal2.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func main() {
+	var tahun int
+	var kabisat bool
+
+	fmt.Print("Tahun: ")
+	fmt.Scan(&tahun)
+
+	if tahun%400 == 0 || (tahun%4 == 0 && tahun%100 != 0) {
+		kabisat = true
+	} else {
+		kabisat = false
+	}
+
+	fmt.Println("Kabisat:", kabisat)
+}
